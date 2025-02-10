4b66fd8fbe500845326299997500e18b54947e45
Jin Igarashi <igarashi.jin@water-gis.com>
2025-02-10 11:46:11 +0900

    vite build setting

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     assetsDir: "assets",
     sourcemap: true,
     lib: {
-      entry: resolve(__dirname, "src/lib/index.ts"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "terrainrgb",
       fileName: (format) => `terrain-rgb.${format}.js`,
       formats: ["es", "umd", "cjs"],
